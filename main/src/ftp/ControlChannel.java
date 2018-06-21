8576d5296511434ab4ffa27c5469d8113539375d
johnngugi <johnngugi777@gmail.com>
2018-06-21 15:29:41 +0300

    Add start and run methods to Control channel

--- main/src/ftp/ControlChannel.java
+++ main/src/ftp/ControlChannel.java
@@ -1,4 +1,26 @@
 package ftp;
 
-public class ControlChannel {
+import java.nio.channels.SocketChannel;
+
+/**
+ * Control channel class
+ */
+public class ControlChannel implements Runnable {
+    private Thread thread;
+
+    public ControlChannel(SocketChannel socketChannel) {
+
+    }
+
+    public void start() {
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
