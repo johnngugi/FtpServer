cd1529b88ee4fc434a1162e52da35ae7f6a242e9
johnngugi <johnngugi777@gmail.com>
2018-06-21 15:27:25 +0300

    Add start, stop and constructor method to Server class

--- main/src/ftp/Server.java
+++ main/src/ftp/Server.java
@@ -1,4 +1,57 @@
 package ftp;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 public class Server {
+    private int port;
+    private boolean running;
+    private String directory;
+    private ServerSocketChannel socket;
+
+    /**
+     * Constructor for Server class
+     * @throws IOException if error occurs while opening or binding a socket
+     */
+    public Server() throws IOException {
+        running = false;
+        this.socket = ServerSocketChannel.open();
+        this.socket.configureBlocking(true);
+        this.socket.socket().bind(new InetSocketAddress(21)); // todo don't hardcode port number
+    }
+
+    /**
+     * Runs the server and starts the control channel on a separate thread
+     */
+    public void start() {
+        if (running) return;
+        try {
+            while (running) {
+                SocketChannel socketChannel = socket.accept();
+                new ControlChannel(socketChannel).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            stop();
+        }
+    }
+
+    /**
+     * Closes the socket and sets it to null
+     */
+    public void stop() {
+        if (this.socket != null) {
+            try {
+                this.socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            this.socket = null;
+            running = false;
+        }
+    }
+
 }
