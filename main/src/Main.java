59d36a008dcb21e5be88d948bdc39e1d075b484a
johnngugi <johnngugi777@gmail.com>
2018-06-21 15:28:53 +0300

    Add code to start application in main method

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,9 +1,15 @@
 import ftp.Server;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        Server server = new Server();
+        try {
+            Server server = new Server();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
