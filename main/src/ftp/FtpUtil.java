cf7dde20e7078c4e0583e234829270f5b6145796
johnngugi <johnngugi777@gmail.com>
2018-06-26 16:05:43 +0300

    Add utility methods

--- main/src/ftp/FtpUtil.java
+++ main/src/ftp/FtpUtil.java
@@ -2,7 +2,10 @@ package ftp;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.AbstractInterruptibleChannel;
+import java.util.List;
 
 public class FtpUtil {
 
@@ -16,14 +19,14 @@ public class FtpUtil {
         } else {
             command = line;
         }
-        return new String[] {command, parameter};
+        return new String[]{command, parameter};
     }
 
     /**
      * Writes a message to the socket channel
      *
      * @param channel the channel to be written to
-     * @param msg the message to be written
+     * @param msg     the message to be written
      * @throws IOException thrown by channel.write() method call
      */
     static void println(SocketChannel channel, String msg) throws IOException {
@@ -32,6 +35,40 @@ public class FtpUtil {
         while (buf.hasRemaining()) {
             if (channel == null) break;
             channel.write(buf);
+        }
+    }
+
+    static void readWriteOperation(ByteChannel readableChannel, ByteChannel writableChannel, ByteBuffer buffer)
+            throws IOException {
+        while (true) {
+            buffer.clear();
+            int readlen = readableChannel.read(buffer);
+            if (readlen < 1)
+                break;
+            buffer.flip();
+            while (buffer.hasRemaining())
+                writableChannel.write(buffer);
+        }
+    }
+
+    static void setTransferComplete(boolean isNegotiable, List<DataConnectionListener> listeners) {
+        if (!isNegotiable) {
+            for (DataConnectionListener l : listeners)
+                l.actionNegotiated(false);
+        } else {
+            for (DataConnectionListener l : listeners)
+                l.transferCompleted(true);
+        }
+    }
+
+    static void releaseChannelResource(AbstractInterruptibleChannel resource) {
+        if (resource != null) {
+            try {
+                resource.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            resource = null;
         }
     }
 
